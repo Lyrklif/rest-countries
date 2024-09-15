beeb27bebc88a314b387e7610c9aecb563c6d723
Alexandr Kochurov <b2b4ly@gmail.com>
2024-09-15 14:43:00 +0600

    feat: app use vue-query

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { VueQueryPlugin } from '@tanstack/vue-query'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(VueQueryPlugin, {
+  queryClientConfig: {
+    defaultOptions: {
+      queries: {
+        retry: 0
+      }
+    }
+  }
+})
 
 app.mount('#app')
