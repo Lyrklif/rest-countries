8b24f8924b4be65c7b2d4599552c140bec3af698
Alexandr Kochurov <b2b4ly@gmail.com>
2024-09-15 17:39:31 +0600

    feat: filter response

--- src/queries/helpers.ts
+++ src/queries/helpers.ts
@@ -2,11 +2,14 @@ export const CACHE_TIME: number = 1000 * 60 * 60 * 24
 
 export const fetcher = async (url: string) => await fetch(url).then((response) => response.json())
 
-const API_URL = 'https://restcountries.com/v3.1'
+const API_URL: string = 'https://restcountries.com/v3.1'
+const LIST_FILTERS: string = '?fields=name,population,capital,region,flags,tld,languages'
+const DETAIN_FILTERS: string =
+  '?fields=name,population,capital,region,subregion,flags,tld,languages,borders'
 
 export const API = {
-  ALL: `${API_URL}/all`,
-  NAME: (name: string): string => `${API_URL}/name/${name}`,
-  CODE: (code: string): string => `${API_URL}/alpha/${code}`,
-  REGION: (region: string): string => `${API_URL}/region/${region}`
+  ALL: `${API_URL}/all${LIST_FILTERS}`,
+  NAME: (name: string): string => `${API_URL}/name/${name}${LIST_FILTERS}`,
+  CODE: (code: string): string => `${API_URL}/alpha/${code}${DETAIN_FILTERS}`,
+  REGION: (region: string): string => `${API_URL}/region/${region}${LIST_FILTERS}`
 }
