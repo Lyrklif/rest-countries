8cfe266eac2ef5bf547a915ece811c79e5956ae7
Alexandr Kochurov <b2b4ly@gmail.com>
2024-09-19 00:21:12 +0600

    feat: add tailwind settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    screens: {
+      sm: '375px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        white: 'hsl(0, 0%, 100%)', // Dark Mode Text & Light Mode Elements
+        'very-light-gray': 'hsl(0, 0%, 98%)', // Light Mode Background
+        'dark-blue': 'hsl(209, 23%, 22%)', // Dark Mode Elements
+        'dark-gray': 'hsl(0, 0%, 52%)', // Light Mode Input
+        'very-dark-blue-light': 'hsl(200, 15%, 8%)', //Light Mode Text
+        'very-dark-blue-dark': 'hsl(207, 26%, 17%)' // Dark Mode Background
+      }
+    },
+    fontFamily: {
+      serif: ['Nunito Sans', 'sans-serif']
+    },
+    fontWeight: {
+      light: 300,
+      semibold: 600,
+      extrabold: 800
+    }
   },
   plugins: []
 }
--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,18 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from 'tailwindcss'
+import webfontDownload from 'vite-plugin-webfont-dl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+    webfontDownload([
+      'https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600;800&display=swap'
+    ])
+  ],
   css: {
     postcss: {
       plugins: [tailwindcss()]
