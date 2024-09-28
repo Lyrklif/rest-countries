29d16ee4260ae80e17d7391e947c5cd9510635a2
Alexandr Kochurov <b2b4ly@gmail.com>
2024-09-28 19:49:06 +0600

    feat: create base input

---
+++ src/components/atoms/BaseInput.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { defineEmits, defineProps } from 'vue'
+
+const props = defineProps<{
+  modelValue: string
+  placeholder?: string
+}>()
+
+const emit = defineEmits(['update:modelValue'])
+
+const onInput = (event: Event) => {
+  const target = event.target as HTMLInputElement
+  emit('update:modelValue', target.value)
+}
+</script>
+
+<template>
+  <input
+    type="search"
+    :value="props.modelValue"
+    @input="onInput"
+    :placeholder="placeholder"
+    class="h-full flex-1 bg-transparent placeholder:text-light-text-secondary dark:placeholder:text-gray-300 focus:outline-none"
+  />
+</template>
--- src/components/molecules/SearchCountry.vue
+++ src/components/molecules/SearchCountry.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { computed, defineEmits, ref } from 'vue'
+import BaseInput from '@/components/atoms/BaseInput.vue'
 
 const MIN_LENGTH = 1
 
@@ -10,20 +11,15 @@ const query = ref('')
 const trimmedQuery = computed(() => query.value.trim())
 const isValidQuery = computed(() => trimmedQuery.value.length >= MIN_LENGTH)
 
-const onInput = () => {
+const onInput = (value: string) => {
   const searchValue = isValidQuery.value ? trimmedQuery.value : ''
   emit('onSearch', searchValue)
+  query.value = value
 }
 </script>
 
 <template>
   <form class="card w-full lg:w-[480px] h-14 space-x-4 px-8 flex items-center rounded-md">
-    <input
-      type="search"
-      v-model="query"
-      @input="onInput"
-      placeholder="Search for a country..."
-      class="h-full flex-1 bg-transparent placeholder:text-light-text-secondary dark:placeholder:text-gray-300 focus:outline-none"
-    />
+    <BaseInput v-model="query" placeholder="Search for a country..." @update:modelValue="onInput" />
   </form>
 </template>
