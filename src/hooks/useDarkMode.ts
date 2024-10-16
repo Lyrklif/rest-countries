0bdbf6d59c70ed2b5d6d071d23d97f81ae0f8e87
Alexandr Kochurov <b2b4ly@gmail.com>
2024-10-16 22:32:49 +0400

    feat: use dark mode by default

--- src/hooks/useDarkMode.ts
+++ src/hooks/useDarkMode.ts
@@ -2,9 +2,10 @@ import { onMounted, ref } from 'vue'
 
 const THEME_CLASS = 'dark'
 const STORAGE_DARK_MODE_KEY = 'darkMode'
+const DEFAULT_DARK_MODE = true
 
 export function useDarkMode() {
-  const isDarkMode = ref<boolean>(true)
+  const isDarkMode = ref<boolean>(DEFAULT_DARK_MODE)
 
   const toggleDarkMode = () => {
     isDarkMode.value = !isDarkMode.value
@@ -14,10 +15,12 @@ export function useDarkMode() {
 
   onMounted(() => {
     const savedMode = localStorage.getItem(STORAGE_DARK_MODE_KEY)
-    if (savedMode === 'true') {
-      isDarkMode.value = true
-      document.documentElement.classList.add(THEME_CLASS)
+
+    if (savedMode !== null) {
+      isDarkMode.value = savedMode === 'true'
     }
+
+    document.documentElement.classList.toggle(THEME_CLASS, isDarkMode.value)
   })
 
   return { isDarkMode, toggleDarkMode }
